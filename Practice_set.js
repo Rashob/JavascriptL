864ca9b28810237079d4999f8e5f8a18ef18cbf4
WASHIUL ISLAM <washiul92@gmail.com>
2022-10-08 09:57:21 +0530

    practice of const let

---
+++ Practice_set.js
@@ -0,0 +1,38 @@
+//create a variable 
+let a = "helloo";
+
+let b = "55";
+
+console.log(a + b);
+
+//to find a type of variable
+
+console.log(typeof a+b);
+
+//Creating a cont and changing   its key add new key
+
+const d ={
+    name: "yoo",
+    section: 1,
+    isprincipal: false
+}
+d ['name']= "tyo"
+d ['friend']= "gib"
+
+console.log(d);
+
+
+
+//  adding dictonary words
+
+const dict={
+
+    areology:'the observation and study of the planet Mars.',
+    Hellacious:'This word is a mix of the word Hell and the suffixcious',
+    Fast_fashion:'An approach to the design, creation,',
+    Supposably:"As may be assumed, imagined, or supposed.",
+    Hygge:"a cozy quality that makes a person feel content and comfortable.",
+    Long_hauler:'Long hauler'
+}
+console.log(dict.areology);
+console.log(dict['Hellacious']);
